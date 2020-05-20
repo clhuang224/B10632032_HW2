af86c610a8a039c8fda0ae3f5171a61a84816ed2
clhuang224 <clhuang224@gmail.com>
2020-05-20 16:02:57 +0800

    Initialize

---
+++ app/src/main/java/tw/com/mydomain/b10632032_hw2/MainActivity.java
@@ -0,0 +1,14 @@
+package tw.com.mydomain.b10632032_hw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/tw/com/mydomain/b10632032_hw2/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package tw.com.mydomain.b10632032_hw2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
